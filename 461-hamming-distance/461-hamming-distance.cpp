3cf0bfd35b3047393d59bac9be98245550c73913
Chinmayi Verma <89857096+chinmayiverma@users.noreply.github.com>
2022-07-24 22:10:16 +0530

    Time: 3 ms (39.16%), Space: 6 MB (27.48%) - LeetHub

---
+++ 461-hamming-distance/461-hamming-distance.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int result = x^y;
+        int count=0;
+        while(result>0)
+        {
+            count+=result & 1;
+            result>>=1;
+        }
+        return count;
+    }
+};
